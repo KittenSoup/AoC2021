2cfbcb836d8dadb51063b813d9c8d32b78222b0f
KittenSoup <freddo@gmail.com>
2021-12-13 15:49:57 +0100

    day nine pt2

--- 9.js
+++ 9.js
@@ -7,6 +7,7 @@ const daynine = () => {
       this.x = x
       this.y = y
       this.value = value
+      this.coord = `${x}x${y}`
     }
   }
 
@@ -16,6 +17,15 @@ const daynine = () => {
 8767896789
 9899965678`
 
+  const getNeighbors = (point, caveMap) => {
+    return [
+      caveMap[`${point.x - 1}x${point.y}`], // to the left
+      caveMap[`${point.x}x${point.y - 1}`], // above
+      caveMap[`${point.x + 1}x${point.y}`], // to the right
+      caveMap[`${point.x}x${point.y + 1}`], // below
+    ]
+  }
+
   const checkIfLowest = (point, caveMap) => {
     isLowest = true
 
@@ -23,12 +33,7 @@ const daynine = () => {
       console.log('found invalid value')
     }
 
-    const neighbors = [
-      caveMap[`${point.x - 1}x${point.y}`], // to the left
-      caveMap[`${point.x}x${point.y - 1}`], // above
-      caveMap[`${point.x + 1}x${point.y}`], // to the right
-      caveMap[`${point.x}x${point.y + 1}`], // below
-    ]
+    const neighbors = getNeighbors(point, caveMap)
 
     neighbors.map((adjacent) => {
       if (adjacent && adjacent.value <= point.value) {
@@ -36,6 +41,26 @@ const daynine = () => {
       }
     })
     return isLowest
+  }
+
+  // should be done in a nicer way I suppose
+  let filled = []
+  let checked = []
+
+  const fillBasin = (startPoint, caveMap) => {
+    const neighbors = getNeighbors(startPoint, caveMap).filter((point) => point && point.value < 9)
+    
+    checked.push(startPoint)
+    neighbors.map((p) => {
+      if (!checked.includes(p)) {
+        filled.push(p)
+        checked.push(p)
+        // also check if p has any neighbors
+        fillBasin(p, caveMap)
+      }
+    })
+
+    return filled
   }
 
   try {
@@ -55,16 +80,32 @@ const daynine = () => {
       })
     })
 
+    const lowPoints = []
+
     const riskMap = Object.keys(caveMap).map((key) => {
       if (caveMap.hasOwnProperty(key)) {
         if (checkIfLowest(caveMap[key], caveMap)) {
+          lowPoints.push(caveMap[key])
           return caveMap[key].value + 1
         }
         return 0
       }
     })
 
-    // console.log(riskMap)
+    console.log('lowPoints: ', lowPoints)
+
+    const basins = lowPoints.map((lowPoint) => {
+      filled = [lowPoint]
+      checked = [lowPoint]
+      return fillBasin(lowPoint, caveMap)
+    })
+    const sizes = basins.map((basin) => {
+      return basin.length
+    })
+
+    sizes.sort((a,b) => b-a)
+
+    console.log('three biggest: ', sizes.slice(0,3).reduce((p,v) => p*v))
 
     console.log(`total risk is: ${riskMap.reduce((p, v) => p+v)}`)
 
